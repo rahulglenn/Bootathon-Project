39c7f9eefd83da719f9f78f89947df33c3906a40
ThulasiRam-S <thulasiramindia@gmail.com>
2020-07-29 21:20:04 +0530

    addemploy frame author and description is changed

--- Bootathon/AddEmploy.java
+++ Bootathon/AddEmploy.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Author(s):Thulasi Ram,Rahul Glenn
+ * Description:this frame gets employee details
  */
 
 /*Dummy*/
@@ -21,10 +20,7 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
-/**
- *
- * @author rahul
- */
+
 public class AddEmploy {
     AddEmploy()
     {
